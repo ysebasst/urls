ac28c1646c56b730dbe9f3d74c1bb0ced6685117
ysebasst <ysebasst@gmail.com>
2021-08-10 04:01:18 +0000

    visual changes and confirmation for delete

--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
+import os
+
+
 class Config:
     SECRET_KEY = "mysec"
-    MONGO_URI = "mongodb+srv://root:apto222@cluster0.7j1kt.mongodb.net/urlsdb?retryWrites=true&w=majority"
+    MONGO_URI = os.environ.get("MONGO_URI")
